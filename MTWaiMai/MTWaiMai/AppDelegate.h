f4f8c04950db6f9c83b72b645d7f955c9c52ee90
justcodingDad <devilmayc4@163.com>
2017-07-30 19:01:03 +0800

    01-美团外卖项目准备

---
+++ MTWaiMai/MTWaiMai/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MTWaiMai
+//
+//  Created by relax on 2017/7/30.
+//  Copyright © 2017年 relax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
